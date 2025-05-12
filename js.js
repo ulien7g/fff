9cf28cac8339db48663618d9d2e6548e6b22b331
Юльян <ipon7200@gmail.com>
2025-05-12 20:02:01 +0300

    add film

--- js.js
+++
@@ -1,8 +0,0 @@
-function toggleAnswer (id) { 
-    const answer = document.getElementById(id); 
-    if (answer.style.display == "none" || !answer.style.display) { 
-    answer.style.display = "block"; 
-    } else { 
-    answer.style.display = "none"; 
-    } 
-}
